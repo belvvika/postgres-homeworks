e9a13dfdd959884c72b123176295fbcba2bdad1f
belvvika <134885414+belvvika@users.noreply.github.com>
2023-12-12 23:06:20 +0300

    update main.py

--- homework-1/main.py
+++ homework-1/main.py
@@ -8,72 +8,30 @@ connect = psycopg2.connect(
     password='12345'
 )
 
-try:
-    with connect:
-        with connect.cursor() as cursor:
-            with open('north_data/employees_data.csv', 'r') as file:
-                reader = csv.reader(file)
-                next(reader)
-                for row in reader:
-                    cursor.execute("insert into emploees (employee_id, first_name, last_name, title, birth_dat, notes) values (%s, %s, %s, %s, %s, %s)", row)
-finally:
-    connect.close()
-#
-#
-# try:
-#     with connect:
-# cursor = connect.cursor()
-# with open('north_data/customers_data.csv', 'r') as file:
-#     reader = csv.reader(file)
-#     next(reader)
-#     for row in reader:
-#         cursor.execute("insert into customers (customer_id, company_name, contact_name) values (%s, %s, %s)", row)
-#     connect.close()
-#
-# cursor = connect.cursor()
-# with open('north_data/orders_data.csv', 'r') as file:
-#     reader = csv.reader(file)
-#     next(reader)
-#     for row in reader:
-#         cursor.execute("insert into orders (order_id, customer_id, employee_id, order_date, ship_city) values (%s, %s, %s, %s, %s)", row)
-#     connect.close()
-#
-#
+
+with connect:
+    with connect.cursor() as cursor:
+        with open('north_data/employees_data.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                cursor.execute("insert into emploees (employee_id, first_name, last_name, title, birth_dat, notes) values (%s, %s, %s, %s, %s, %s)", row)
 
 
-connect = psycopg2.connect(
-    host='localhost',
-    database='north',
-    user='postgres',
-    password='12345'
-)
-try:
-    with connect:
-        with connect.cursor() as cursor:
-            with open('north_data/customers_data.csv', 'r') as file:
-                reader = csv.reader(file)
-                next(reader)
-                for row in reader:
-                    cursor.execute("insert into customers (customer_id, company_name, contact_name) values (%s, %s, %s)", row)
-finally:
-    connect.close()
+with connect:
+    with connect.cursor() as cursor:
+        with open('north_data/customers_data.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                cursor.execute("insert into customers (customer_id, company_name, contact_name) values (%s, %s, %s)", row)
 
 
+with connect:
+    with connect.cursor() as cursor:
+        with open('north_data/orders_data.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                cursor.execute("insert into orders (order_id, customer_id, employee_id, order_date, ship_city) values (%s, %s, %s, %s, %s)", row)
 
-
-connect = psycopg2.connect(
-    host='localhost',
-    database='north',
-    user='postgres',
-    password='12345'
-)
-try:
-    with connect:
-        with connect.cursor() as cursor:
-            with open('north_data/orders_data.csv', 'r') as file:
-                reader = csv.reader(file)
-                next(reader)
-                for row in reader:
-                    cursor.execute("insert into orders (order_id, customer_id, employee_id, order_date, ship_city) values (%s, %s, %s, %s, %s)", row)
-finally:
-    connect.close()
